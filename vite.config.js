c5241a19fbb7eb26969eb7695cefa79befd5db1f
srushti <srushtipatel4040@gmail.com>
2024-12-05 18:25:20 +0530

    fourth1234

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,11 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        require('autoprefixer')(),
+      ],
+    },
+  },
 });
