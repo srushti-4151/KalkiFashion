4b73e4d9cfd276e5bef2fb40a331bcef37849528
srushti <srushtipatel4040@gmail.com>
2024-12-05 18:22:48 +0530

    fourth123

--- vite.config.js
+++ vite.config.js
@@ -11,14 +11,4 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [
-        require('postcss-import')(),
-        require('postcss-preset-env')({
-          stage: 1, // Enables modern CSS features
-        }),
-      ],
-    },
-  },
 });
